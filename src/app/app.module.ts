0720ea873ce6622e5a71237dcfc8db588e3e82fb
sfitzgerald16 <misfitz616@gmail.com>
2017-09-11 21:22:57 -0700

    added more samples and began contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -30,6 +31,8 @@ import { SocialMediaComponent } from './worksamples/social-media/social-media.co
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  form;
+  processing;
+  message;
+  messageClass;
+
+  constructor(
+    private formBuilder: FormBuilder,
+
+  ) {
+    this.createForm()
+   }
+
+   createForm() {
+     this.form = this.formBuilder.group({
+       name: ['', Validators.compose([
+         Validators.required,
+       ])],
+       email: ['', Validators.compose([
+        Validators.required,
+        this.validateEmail
+       ])],
+       comment: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(10)
+       ])]
+    });
+   }
+
+  disableForm() {
+    this.form.controls['email'].disable();
+    this.form.controls['name'].disable();
+    this.form.controls['comment'].disable();
+    }
+
+  enableForm() {
+    this.form.controls['email'].enable();
+    this.form.controls['name'].enable();
+    this.form.controls['comment'].enable();
+  }
+
+  onSubmit() {
+    this.processing = true;
+    this.disableForm();
+  }
+
+
+   validateEmail(controls) {
+   const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+   if (regExp.test(controls.value)) {
+     return null;
+     } else {
+       return {'validateEmail': true}
+     }
+   }
+
 
   ngOnInit() {
   }
